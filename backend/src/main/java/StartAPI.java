da53177900f0a5f2b0b1453cb4a2f8d799369023
Oscar Strandmark <oscar.strandmark1@gmail.com>
2020-01-09 11:52:20 +0100

    Emptied master from PH code

--- backend/src/main/java/StartAPI.java
+++ backend/src/main/java/StartAPI.java
@@ -5,11 +5,6 @@ public class StartAPI {
     public static void main(String[] args) {
         port(5050);
 
-        Spotify spotify = new Spotify();
-        SR sr = new SR();
-
-        get("/",(request,response) -> {
-            return sr.getCurrentlyPlaying(request.attribute("channelID"));
-        });
+        
     }
 }
