09e9414fd2833ff96056daa0c70f0430d0cc7e30
Oscar Strandmark <oscar.strandmark1@gmail.com>
2020-01-09 12:16:45 +0100

    addToLib

--- backend/src/main/java/StartAPI.java
+++ backend/src/main/java/StartAPI.java
@@ -1,10 +1,18 @@
-import static spark.Spark.port;
-import static spark.Spark.get;
+import controllers.AddToLibraryController;
+
+import static spark.Spark.*;
 
 public class StartAPI {
     public static void main(String[] args) {
         port(5050);
 
-        
+        AddToLibraryController addToLibraryController = new AddToLibraryController();
+        path("/spotify", () -> {
+
+            //Search endpoints
+            path("/library", () -> {
+                put("/addto", (req,res) -> addToLibraryController.addToLibrary(req.body()));
+            });
+        });
     }
 }
---
+++ backend/src/main/java/controllers/AddToLibraryController.java
@@ -0,0 +1,24 @@
+package controllers;
+
+import com.google.gson.Gson;
+import models.AddToLibrary;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+
+public class AddToLibraryController {
+    public String addToLibrary(String json){
+        AddToLibrary libraryData = new Gson().fromJson(json,AddToLibrary.class);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Authorization", libraryData.getAuth());
+        
+        HttpEntity<String> reqEntity = new HttpEntity<String>("",headers);
+        ResponseEntity<String> resEntity = new RestTemplate().exchange("https://api.spotify.com/v1/me/tracks?ids=" + libraryData.getTrack_id()
+                , HttpMethod.PUT ,reqEntity ,String.class);
+
+        return resEntity.getBody();
+    }
+}
